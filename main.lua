2b0675a891f46f222b1006645653bcabe6faa141
Kamil <kamilek41@gmail.com>
2025-12-11 20:12:18 +0100

    Added ball seving

--- main.lua
+++ main.lua
@@ -11,12 +11,28 @@ PADDLE_SPACING_TO_EDGE = 5
 PADDLE_SPEED = 200
 PADDLE_WIDTH = 5
 PADDLE_HEIGHT = 30
-BALL_SIZE = 4
+local paddleY = 5
 
-local paddlePosition = 5
+BALL_SIZE = 4
+local ballDx = 0
+local ballDy = 0
+local ballX
+local ballY
+
+local player1Score = 0
+local player2Score = 0
+
+
+local function resetBallPosition()
+    ballX = VIRTUAL_WIDTH / 2 - BALL_SIZE
+    ballY = VIRTUAL_HEIGHT / 2 + BALL_SIZE
+end
 
 function love.load()
+    math.randomseed(os.time())
+    resetBallPosition()
     smallFont = love.graphics.newFont("font.ttf", 8)
+    scoreFont = love.graphics.newFont("font.ttf", 32)
     love.graphics.setFont(smallFont)
     love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -29,10 +45,9 @@ end
 function love.draw()
     push:start()
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
-    love.graphics.printf("Hello pong!", 0, 20, VIRTUAL_WIDTH, "center")
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2 + BALL_SIZE, BALL_SIZE, BALL_SIZE)
+    love.graphics.rectangle('fill', ballX, ballY, BALL_SIZE, BALL_SIZE)
     -- Renders left paddle
-    love.graphics.rectangle('fill', PADDLE_SPACING_TO_EDGE, paddlePosition, PADDLE_WIDTH, PADDLE_HEIGHT)
+    love.graphics.rectangle('fill', PADDLE_SPACING_TO_EDGE, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT)
     -- Renders right paddle
     love.graphics.rectangle(
         'fill',
@@ -42,20 +57,34 @@ function love.draw()
         PADDLE_HEIGHT
     )
 
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     push:finish()
 end
 
 function love.update(dt)
     if love.keyboard.isDown("w") then
-        paddlePosition = paddlePosition - PADDLE_SPEED * dt
+        paddleY = paddleY - PADDLE_SPEED * dt
     end
     if love.keyboard.isDown("s") then
-        paddlePosition = paddlePosition + PADDLE_SPEED * dt
+        paddleY = paddleY + PADDLE_SPEED * dt
     end
+
+    -- Update ball movement
+    ballX = ballX + ballDx * dt
+    ballY = ballY + ballDy * dt
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    end
+    if key == 'space' then
+        resetBallPosition()
+        print(math.random(0, 1))
+        ballDx = math.random(0, 1) == 0 and 100 or -100
+        ballDy = math.random(-50, 50)
     end
 end
